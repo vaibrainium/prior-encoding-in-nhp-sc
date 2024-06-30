e6a5247a69c6e1a043c59e421e10cb24b4bd2cef
vaibrainium <vaibhavt459@gmail.com>
2024-06-30 03:19:54 +0000

    chore: notebook package created for better imports

---
+++ notebooks/imports.py
@@ -0,0 +1,10 @@
+
+from pathlib import Path
+import numpy as np
+import pandas as pd
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+pd.set_option("display.max_columns", None)
+
+import matplotlib.pyplot as plt
+
