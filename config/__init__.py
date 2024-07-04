520cfbe39de0ae79fd9d234783081ca645807741
vaibrainium <vaibhavt459@gmail.com>
2024-07-04 00:25:31 +0000

    config import updated

--- config/__init__.py
+++ config/__init__.py
@@ -6,12 +6,20 @@
 
 
 from pathlib import Path
+import json
 from omegaconf import OmegaConf
 
 # Load the YAML configuration file in config directory
 def load_config(filename):
     return OmegaConf.load(Path(__file__).parent / filename)
 
+def load_json(filename):
+    with open(Path(__file__).parent / filename, 'r') as file:
+        return json.load(file)
+
+
 # Example of accessing a specific configuration file
 dir_config = load_config('dir-config.yaml')
 main_config = load_config('main.yaml')
+
+s_probe_path = Path(__file__).parent / 's-probe.json'
