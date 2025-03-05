c1134b6e5e2ebc7b24c2cd2fb03de9569a15dfd3
vaibrainium <vaibhavt459@gmail.com>
2025-03-05 06:04:41 +0000

    chore: linting

--- config/__init__.py
+++ config/__init__.py
@@ -9,18 +9,20 @@ from pathlib import Path
 import json
 from omegaconf import OmegaConf
 
+
 # Load the YAML configuration file in config directory
 def load_config(filename):
-    return OmegaConf.load(Path(__file__).parent / filename)
+	return OmegaConf.load(Path(__file__).parent / filename)
+
 
 def load_json(filename):
-    with open(Path(__file__).parent / filename, 'r') as file:
-        return json.load(file)
+	with open(Path(__file__).parent / filename, "r") as file:
+		return json.load(file)
 
 
 # Example of accessing a specific configuration file
-dir_config = load_config('dir-config.yaml')
-main_config = load_config('main.yaml')
-ephys_config = load_config('ephys-config.yaml')
+dir_config = load_config("dir-config.yaml")
+main_config = load_config("main.yaml")
+ephys_config = load_config("ephys-config.yaml")
 
-s_probe_path = Path(__file__).parent / 's-probe.json'
+s_probe_path = Path(__file__).parent / "s-probe.json"
