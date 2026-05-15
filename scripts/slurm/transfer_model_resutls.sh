c7a17ad8dadced082f19561977f29a67b09d46cf
vaibrainium <vaibhavt459@gmail.com>
2026-05-15 02:03:17 +0000

    chore: scp commands

--- scripts/slurm/transfer_model_resutls.sh
+++ scripts/slurm/transfer_model_resutls.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-scp -r vthakur@klone.hyak.uw.edu:/gscratch/walkerlab/vaibhav/nhp-prior-data/processed/ddm /mnt/data/processed/
+# get environment variables
+set -a
+source ../../.env
+set +a
+
+# create the singularity image
+singularity build nhp-prior.sif docker-daemon://test:latest
+
+# transfer to the cluster
+scp nhp-prior.sif ${CLUSTER_USER}@${CLUSTER_HOST}:${CLUSTER_DATA_PATH}/processed/
+scp ${CONTAINER_DATA_PATH}/ddm/behavior_data.csv ${CLUSTER_USER}@${CLUSTER_HOST}:${CLUSTER_DATA_PATH}/processed/ddm/
+
+# transfer from the cluster
+scp -r ${CLUSTER_USER}@${CLUSTER_HOST}:${CLUSTER_DATA_PATH}/processed/ddm/ ${CONTAINER_DATA_PATH}/ddm/
